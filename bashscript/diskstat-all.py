fd00d183d90c1a3f91f86dca6fd2f46d9924110c
mocham <45742107+mocham@users.noreply.github.com>
2020-08-08 02:23:32 -0400

    Newly added

---
+++ bashscript/diskstat-all.py
@@ -0,0 +1,14 @@
+import sys, math
+
+with open ('/proc/diskstats', 'r') as f:
+    for l in f.readlines():
+        sp = l.split('sd')
+        if len(sp) != 2:
+            continue
+        s = sp[1]
+        np = s.split(' ')
+        if len(np) != 12:
+            continue
+        #if len(np[0]) != 1:
+        #    continue
+        sys.stdout.write('sd' + np[0] + ' ' + str(math.floor(int(np[7])/1024/2)) + '; ')
